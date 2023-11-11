e9f52d698e8309720d82ba386f93748e451cc0cd
Nedko Dimitrov <122704901+nedkodimitrov@users.noreply.github.com>
2023-11-11 23:23:11 +0200

    Fix urls.py.

--- cs50w_music/cs50w_music/urls.py
+++ cs50w_music/cs50w_music/urls.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from songs import views
 
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserProfileViewSet)
-router.register(r'songs', views.SongViewSet)
-router.register(r'playlists', views.PlaylistViewSet)
-router.register(r'albums', views.AlbumViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', include(router.urls)),
-    path('songs/', include('songs.urls')),
+    path('', include('songs.urls')),
 ]
--- cs50w_music/songs/urls.py
+++ cs50w_music/songs/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-from .views import PlaySongView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from songs import views
 
 
 app_name = 'songs'
 
+router = DefaultRouter()
+router.register(r'users', views.UserProfileViewSet)
+router.register(r'songs', views.SongViewSet)
+router.register(r'playlists', views.PlaylistViewSet)
+router.register(r'albums', views.AlbumViewSet)
+
 urlpatterns = [
-    path('<int:song_id>/play/', PlaySongView.as_view(), name='play-song'),
+    path('', include(router.urls)),
+    path('<int:song_id>/play/', views.PlaySongView.as_view(), name='play-song'),
 ]
