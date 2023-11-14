7ff41c2a7d1d511201791cc264dfa11be395e856
Nedko Dimitrov <122704901+nedkodimitrov@users.noreply.github.com>
2023-11-14 19:39:53 +0200

    Enable session authentication for the drf browsable API.

--- cs50w_music/songs/tests.py
+++ cs50w_music/songs/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class IndexTest(TestCase):
+    def test_index(self):
+
+        # Set up client to make requests
+        c = Client()
+
+        # Send get request to index page and store response
+        response = c.get("/")
+
+        # Make sure status code is 200
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Make sure three flights are returned in the context
+        #self.assertEqual(response.context["flights"].count(), 3)
